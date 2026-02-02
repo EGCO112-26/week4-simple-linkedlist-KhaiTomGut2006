b6a5647c8ebca89814418cf8d30ec8c6da72bb83
Tanawat Wongwad <63237781+KhaiTomGut2006@users.noreply.github.com>
2026-02-02 15:26:15 +0700

    Exercise 1 2 3 4

--- main.c
+++ main.c
@@ -5,59 +5,86 @@
 //  Created by Mingmanas Sivaraksa on 4/2/2566 BE.
 //
 
+#include "node.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "node.h"
 
-int main(int argc, const char * argv[]) {
-    int c=5;
-    struct node a,b,*head ;
-    a.value = c;
-    a.next=&b;
-    head=&a;
-    b.value=head->value+3;
+int main(int argc, const char *argv[]) {
+  struct node a, b, *head;
+  a.value = 5;
+  a.next = &b;
+  head = &a;
+  b.value = head->value + 3;
 
-    printf("%d\n", head ->value ); //what value for 5
-    printf("%d\n", head ->next->value ); //what value for 8
-/*  Exercise I
-    1. Add 1 more than at the end
-    2. Add value(11)
-    3. Make next become NULL
- */
-    
-/*  Exercise II
-        1. Add 1 more than at the begining!!!!
-        2. Add value (2)
-        
-*/
-    typedef struct node* NodePtr;
-    NodePtr tmp=head; //add temp value to faciliate
-        
-    /*  Exercise III Use loop to print everything
-        int i,n=5;
-        for(i=0;i<n;i++){
-            printf("%3d", tmp->value);
+  struct node d;
+  b.next = &d;
+  d.value = 11;
+  d.next = NULL;
+
+  struct node e;
+  head = &e;
+  e.value = 2;
+  e.next = &a;
+
+  /*
+  printf("%d\n", head->value);       // what value for 5
+  printf("%d\n", head->next->value); // what value for 8
+  printf("%d\n", head->next->next->value);
+  printf("%d\n", head->next->next->next->value);
+  */
+
+  /*  Exercise I
+      1. Add 1 more than at the end
+      2. Add value(11)
+      3. Make next become NULL
+   */
+
+  /*  Exercise II
+          1. Add 1 more than at the begining!!!!
+          2. Add value (2)
+
+  */
+  typedef struct node *NodePtr;
+  NodePtr tmp = head; // add temp value to faciliate
+
+  int i, n = 4;
+  printf("For Loop\n");
+  for (i = 0; i < n; i++) {
+    printf("%3d\n", tmp->value);
+    tmp = tmp->next;
+  }
+  printf("While Loop\n");
+  tmp = head;
+  while (tmp != NULL) {
+    printf("%3d\n", tmp->value);
+    tmp = tmp->next;
+  }
+
+  /*  Exercise III Use loop to print everything
+      int i,n=5;
+      for(i=0;i<n;i++){
+          printf("%3d", tmp->value);
+        // What is missing???
+      }
+  */
+
+  /*  Exercise IV change to while loop!! (you can use NULL to help)
+
+        while(){
+           printf("%3d", tmp->value);
           // What is missing???
-        }
-    */
-    
-   /*  Exercise IV change to while loop!! (you can use NULL to help)
-       
-         while(){
-            printf("%3d", tmp->value);
-           // What is missing???
-        }
-    */
-    
- /*  Exercise V Use malloc to create all nodes, instead of create a struct!!
-         //use a loop to help
-          
-     */
+       }
+   */
 
-    /*  Exercise VI Free all node !!
-         //use a loop to help
-          
-     */
-    
-    return 0;
+  /*  Exercise V Use malloc to create all nodes, instead of create a struct!!
+          //use a loop to help
+
+      */
+
+  /*  Exercise VI Free all node !!
+       //use a loop to help
+
+   */
+
+  return 0;
 }
